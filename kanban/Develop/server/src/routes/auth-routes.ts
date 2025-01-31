c357fd44ad400c7876a24d1224f46609815bdb57
Apgash <ashtonvilkosi@gmail.com>
2025-01-31 15:46:37 -0500

    completes auth.ts and auth routes

--- kanban/Develop/server/src/routes/auth-routes.ts
+++ kanban/Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,24 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  const user = await User.findOne({
+    where: {username},
+  });
+  if (!user) {
+    return res.sendStatus(404);
+  }
+
+  const passwordIsvalid = await bcrypt.compare(password, user.password);
+  if (!passwordIsvalid) {
+    return res.sendStatus(401);
+  }
+
+  const secretKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({username}, secretKey);
+  return res.json({token});
 };
 
 const router = Router();
