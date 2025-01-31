b2cb8ea31fa4b631f74944125bd6b236f51587ba
Apgash <ashtonvilkosi@gmail.com>
2025-01-31 15:54:29 -0500

    updates auth routes and completes auth.ts and authAPI

--- kanban/Develop/client/src/utils/auth.ts
+++ kanban/Develop/client/src/utils/auth.ts
@@ -3,28 +3,46 @@ import { JwtPayload, jwtDecode } from 'jwt-decode';
 class AuthService {
   getProfile() {
     // TODO: return the decoded token
+    return jwtDecode(this.getToken()) as JwtPayload;
   }
 
   loggedIn() {
     // TODO: return a value that indicates if the user is logged in
+    const token = this.getToken();
+    return !!token && !this.isTokenExpired(token);
   }
   
   isTokenExpired(token: string) {
     // TODO: return a value that indicates if the token is expired
+    try {
+      const decoded = jwtDecode(token) as JwtPayload;
+      if (decoded.exp && decoded.exp * 1000 < Date.now()) {
+        return true;
+      }
+      return false;
+    } catch (error) {
+      return false;
+    }
   }
 
   getToken(): string {
     // TODO: return the token
+    const loggedUser = localStorage.getItem('id_token');
+    return loggedUser ? loggedUser : '';
   }
 
   login(idToken: string) {
     // TODO: set the token to localStorage
+    localStorage.setItem('id_token', idToken);
     // TODO: redirect to the home page
+    window.location.assign('/');
   }
 
   logout() {
     // TODO: remove the token from localStorage
+    localStorage.removeItem('id_token');
     // TODO: redirect to the login page
+    window.location.assign('/login');
   }
 }
 
